51afa1b5406dce13db51bc9a54129576ef2676e1
xuset <xuset1@gmail.com>
2013-12-06 11:39:01 -0600

    Added HashMap storing.

--- src/tSquare/imaging/AnimatedSprite.java
+++ src/tSquare/imaging/AnimatedSprite.java
@@ -3,25 +3,37 @@ package tSquare.imaging;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 public class AnimatedSprite {
-	public final Sprite[] sprites;
+	private static final HashMap<String, AnimatedSprite> storedSprites = new HashMap<String, AnimatedSprite>();
 	
+	public static AnimatedSprite ADD(String key, AnimatedSprite as) {
+		storedSprites.put(key, as);
+		return as;
+	}
+	
+	public static AnimatedSprite GET(String key) {
+		return storedSprites.get(key);
+	}
+	
+	public final Sprite[] sprites;
+	public final int total;
 	
 	public AnimatedSprite(String url, BufferedImage image, int rows, int columns, int total) {
 		sprites = new Sprite[total];
-		createContents(url, image, rows, columns, total);
+		this.total = createContents(url, image, rows, columns, total);
 	}
 	
 	public AnimatedSprite(String url, int rows, int columns, int total) {
 		sprites = new Sprite[total];
 		BufferedImage img = loadImage(url);
-		createContents(url, img, rows, columns, total);
+		this.total = createContents(url, img, rows, columns, total);
 	}
 	
-	private void createContents(String url, BufferedImage image, int rows, int columns, int total) {
+	private int createContents(String url, BufferedImage image, int rows, int columns, int total) {
 		int widthPer = image.getWidth() / columns;
 		int heightPer = image.getHeight() / rows;
 		int count = 0;
@@ -36,6 +48,7 @@ public class AnimatedSprite {
 				count++;
 			}
 		}
+		return count;
 	}
 	
 	private BufferedImage loadImage(String url) {
