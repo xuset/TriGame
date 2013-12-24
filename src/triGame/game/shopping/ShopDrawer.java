c102764a33d0b1e9022d5cfb9345c71e4f471c02
xuset <xuset1@gmail.com>
2013-12-24 09:41:19 -0600

    Added a delay to the point gain animation.

--- src/triGame/game/shopping/ShopDrawer.java
+++ src/triGame/game/shopping/ShopDrawer.java
@@ -11,9 +11,13 @@ import triGame.game.entities.PointParticle;
 
 public class ShopDrawer {
 	private static final int particlesPerPoints = 4;
+	private static final int particleSpawnDelay = 10;
 	private static final int drawX = 10, drawY = 5;
 	
 	private final ArrayList<Particle> particles = new ArrayList<Particle>(50);
+	
+	private int particlesToSpawn = 0;
+	private long nextParticleSpawn = 0;
 	
 	
 	public ShopDrawer(Observer<ShopManager> shop) {
@@ -27,14 +31,23 @@ public class ShopDrawer {
 			int diff = t.getPointCount() - lastPoints;
 			lastPoints = t.getPointCount();
 			int mult = diff / particlesPerPoints;
-			for (int i = 0; i < mult; i++) {
-				double angle = Math.random() * Math.PI * 2;
-				particles.add(new PointParticle.Flying(drawX, drawY, angle, 50));
-			}
+			if (mult > 0)
+				particlesToSpawn += mult;
 		}
 	}
 	
 	public void draw(int frameDelta, Graphics2D g, ViewRect rect) {
+		if (particlesToSpawn > 50)
+			nextParticleSpawn = 0;
+		
+		if (particlesToSpawn > 0 && nextParticleSpawn < System.currentTimeMillis()) {
+
+			double angle = Math.random() * Math.PI * 2;
+			particles.add(new PointParticle.Flying(drawX, drawY, angle, 50));
+			particlesToSpawn--;
+			nextParticleSpawn = System.currentTimeMillis() + particleSpawnDelay;
+		}
+		
 		for (Iterator<Particle> it = particles.iterator(); it.hasNext(); ) {
 			Particle p = it.next();
 			if (p.isExpired())
