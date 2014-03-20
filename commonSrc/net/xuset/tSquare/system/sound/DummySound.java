ea00ccdac3e08870d1e64dafa2b38193e86f0c92
Austin Middleton <xuset1@gmail.com>
2014-03-20 11:02:44 -0500

    Added a sound object that does nothing. Should be used when there is an error loading a sound.

---
+++ commonSrc/net/xuset/tSquare/system/sound/DummySound.java
@@ -0,0 +1,20 @@
+package net.xuset.tSquare.system.sound;
+
+public class DummySound implements ISound {
+
+	@Override
+	public void play() {
+		//Do nothing
+	}
+
+	@Override
+	public void stop() {
+		//Do nothing
+	}
+
+	@Override
+	public void close() {
+		//Do nothing
+	}
+
+}
