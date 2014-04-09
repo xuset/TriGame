7f8d36c6994d3ccf9a4e419cc79b2e83e06d0968
Austin Middleton <xuset1@gmail.com>
2014-04-09 16:46:30 -0500

    Improved the intro form

--- commonSrc/net/xuset/triGame/intro/IntroConnected.java
+++ commonSrc/net/xuset/triGame/intro/IntroConnected.java
@@ -3,6 +3,7 @@ package net.xuset.triGame.intro;
 import net.xuset.objectIO.connections.sockets.ServerEventListener;
 import net.xuset.objectIO.connections.sockets.groupNet.client.GroupClientCon;
 import net.xuset.objectIO.netObject.StandardObjUpdater;
+import net.xuset.tSquare.imaging.TsColor;
 import net.xuset.tSquare.system.Network;
 import net.xuset.tSquare.ui.Alignment;
 import net.xuset.tSquare.ui.Axis;
@@ -14,26 +15,29 @@ import net.xuset.triGame.game.GameInfo;
 import net.xuset.triGame.game.GameInfo.NetworkType;
 
 public class IntroConnected implements IntroForm{
+	private static final String connectedText = "Connected. Waiting on host to start.";
+	private static final String errorText = "Could not connect to host.";
 	private static final String playerText = "Players joined: ";
 	
 	private final UiForm frmMain = new UiForm();
 	private final UiLabel lblPlayers = new UiLabel();
+	private final UiLabel lblStatus = new UiLabel(connectedText);
 	private final ConnectionListener listener = new ConnectionListener();
 	
 	private Network network = null;
 	private NetworkGameStarter gameStarter = null;
+	private boolean createOnNext = false;
 	
 	public IntroConnected() {
-		UiLabel lblConnected = new UiLabel();
-		lblConnected.setText("Connected. Waiting on host to start.");
 		frmMain.setLayout(new UiQueueLayout(20, 20, frmMain));
 		frmMain.getLayout().setOrientation(Axis.Y_AXIS);
 		frmMain.getLayout().setAlignment(Axis.X_AXIS, Alignment.CENTER);
-		frmMain.getLayout().add(lblConnected);
+		frmMain.getLayout().add(lblStatus);
 		frmMain.getLayout().add(lblPlayers);
 	}
 	
 	public void setNetwork(Network network) {
+		setStatus(false, connectedText);
 		disconnectNetwork();
 		this.network = network;
 		network.getClientInstance().watchEvents(listener);
@@ -49,7 +53,12 @@ public class IntroConnected implements IntroForm{
 	@Override
 	public GameInfo getCreatedGameInfo() {
 		if (gameStarter != null && gameStarter.hasGameStarted()) {
-			return new GameInfo(network, gameStarter.getGameType(), NetworkType.JOIN);
+			if (createOnNext)
+				return new GameInfo(network, gameStarter.getGameType(), NetworkType.JOIN);
+			else {
+				createOnNext = true;
+				setStatus(false, "Loading game...");
+			}
 		}
 		return null;
 	}
@@ -66,7 +75,17 @@ public class IntroConnected implements IntroForm{
 
 	@Override
 	public void update() {
-		
+		if (!network.getClientInstance().isConnectedToServer() &&
+				!lblStatus.getText().equals(errorText)) {
+			
+			setStatus(true, errorText);
+			lblPlayers.setText("");
+		}
+	}
+	
+	private void setStatus(boolean error, String text) {
+		lblStatus.setText(text);
+		lblStatus.setForeground(error ? TsColor.red : TsColor.black);
 	}
 	
 	private void disconnectNetwork() {
@@ -79,7 +98,10 @@ public class IntroConnected implements IntroForm{
 	}
 	
 	private void updatePlayerCount() {
-		lblPlayers.setText(playerText + network.hub.getConnectionCount());
+		if (network.getClientInstance().isConnectedToServer())
+			lblPlayers.setText(playerText + (network.hub.getConnectionCount()));
+		else
+			lblPlayers.setText("");
 	}
 	
 	private class ConnectionListener implements ServerEventListener<GroupClientCon> {
