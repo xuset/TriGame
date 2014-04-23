23206eaaed8efc87452bcffcc6ad8fe0343bfc83
Austin Middleton <xuset1@gmail.com>
2014-04-22 20:29:34 -0500

    Increased version numbers

--- commonSrc/net/xuset/triGame/Params.java
+++ commonSrc/net/xuset/triGame/Params.java
@@ -16,7 +16,7 @@ public class Params {
 	public static final String WEBSITE_URL = "http://wwww.xuset.net";
 	public static final String GITHUB_URL = "https://github.com/xuset/triGame";
 	public static final String GAME_NAME = "PolyDefense";
-	public static final Version VERSION = new Version(1, 2, 0);
+	public static final Version VERSION = new Version(1, 3, 0);
 	public static final boolean CHECK_FOR_UPDATES = !DEBUG_MODE;
 	public static final String UNUSED = null; //Only used for irony.
 	public static final String SCORE_SUBMIT_URL =
