936f05473a9958f80381c5065d7a94c48414fc57
Austin Middleton <xuset1@gmail.com>
2014-05-03 08:43:12 -0500

    Rounded progress on UiProgressBar.

--- commonSrc/net/xuset/tSquare/ui/UiSlider.java
+++ commonSrc/net/xuset/tSquare/ui/UiSlider.java
@@ -19,39 +19,12 @@ public class UiSlider extends UiProgressBar {
 		super.recieveMouseEvent(e, x, y);
 		
 		if (e.pointer.isPressed()) {
-			setProgress(x / getWidth());
+			int seg = 30;
+			double newProg = x / getWidth() + 0.5 / seg;
+			newProg = Math.floor(newProg * seg) / seg;
+			setProgress(newProg);
 			if (listener != null)
 				listener.onChange(getProgress());
 		}
 	}
-
-	/*private void drawBar(IGraphics g) {
-		final float gutter = 1.0f;
-		
-		g.setColor(getBackground().darker());
-		g.fillRect(getX(), getY(), getWidth(), getHeight());
-		g.setColor(getBackground());
-		g.fillRect(getX() + gutter, getY() + gutter,
-				getWidth() - 2 * gutter, getHeight() - 2 * gutter);
-		
-		g.setColor(getForeground());
-		float w = (float) ((getWidth() - 2 * gutter) * position);
-		g.fillRect(getX() + gutter, getY() + gutter, w, getHeight() - 2 * gutter);
-	}
-	
-	private void drawMarker(IGraphics g) {
-		final float markerW = 5.0f;
-		final float markerH = getHeight();
-		
-		float markerX = (float) (getX() + getWidth() * position - markerW / 2);
-		float markerY = getY();
-		
-		float middleX = markerX + markerW / 2;
-		float middleY = markerY + markerH;
-		
-		g.setColor(getForeground().lighter());
-		g.drawLine(markerX, markerY, middleX, middleY);
-		g.drawLine(markerX + markerW, markerY, middleX, middleY);
-		
-	}*/
 }
