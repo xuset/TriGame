4222479be41d3fa38d77d04628439d9cd8950d97
Austin Middleton <xuset1@gmail.com>
2014-05-06 08:33:05 -0500

    Added an ENABLE_MULTIPLAYER option to Params

--- commonSrc/net/xuset/triGame/Params.java
+++ commonSrc/net/xuset/triGame/Params.java
@@ -17,15 +17,20 @@ public class Params {
 	
 	public static final boolean IS_ANDROID = false;
 	public static final boolean DEBUG_MODE = true;
-	public static final String GAME_NAME = "PolyDefense";
-	public static final Version VERSION = new Version(1, 3, 0);
 	public static final boolean CHECK_FOR_UPDATES = !DEBUG_MODE && !IS_ANDROID;
-	public static final String UNUSED = null; //Only used for irony.
+	public static final boolean ALLOW_DOUBLE_CLICK = !IS_ANDROID;
+	public static final boolean ENABLE_MULTIPLAYER = !IS_ANDROID;
+	
+	public static final Version VERSION = new Version(1, 3, 0);
+	public static final Level LOG_LEVEL = Level.WARNING;
+	
+	public static final String GAME_NAME = "PolyDefense";
+	public static final String SCORE_KEY = "no esta aqui";
+
 	public static final String SCORE_SUBMIT_URL =
 			"http://xuset.net/polyDefense/scores/scoreSubmit.php";
-	public static final String SCORE_KEY = "no esta aqui";
-	public static final Level LOG_LEVEL = Level.WARNING;
 	public static final String ERROR_REPORT_URL =
 			"http://www.xuset.net/polyDefense/bugs/submit.php";
-	public static final boolean ALLOW_DOUBLE_CLICK = !IS_ANDROID;
+	
+	public static final String UNUSED = null; //Only used for irony.
 }
--- commonSrc/net/xuset/triGame/intro/IntroMain.java
+++ commonSrc/net/xuset/triGame/intro/IntroMain.java
@@ -7,6 +7,7 @@ import net.xuset.tSquare.ui.UiComponent;
 import net.xuset.tSquare.ui.UiForm;
 import net.xuset.tSquare.ui.layout.UiQueueLayout;
 import net.xuset.tSquare.util.Observer;
+import net.xuset.triGame.Params;
 import net.xuset.triGame.game.GameInfo;
 
 class IntroMain implements IntroForm {
@@ -20,14 +21,15 @@ class IntroMain implements IntroForm {
 		UiQueueLayout mainLayout = new UiQueueLayout(20, 5, frmMain);
 		frmMain.setLayout(mainLayout);
 		
-		btnSolo = new UiButton("Solo");
+		btnSolo = new UiButton(Params.ENABLE_MULTIPLAYER ? "Solo" : "Start game");
 		btnMulti = new UiButton("Multiplayer");
 		
 		btnSolo.addMouseListener(new BtnSoloAction());
 		btnMulti.addMouseListener(new BtnMultiAction());
 		
 		mainLayout.add(btnSolo);
-		mainLayout.add(btnMulti);
+		if (Params.ENABLE_MULTIPLAYER)
+			mainLayout.add(btnMulti);
 	}
 
 	@Override
