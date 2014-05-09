5d73370e16f80a9f729abbc9f8a5fb83838a5adc
Austin Middleton <xuset1@gmail.com>
2014-05-08 20:25:42 -0500

    Fixing SoundPool issues

--- androidSrc/net/xuset/tSquare/system/sound/AndroidSound.java
+++ androidSrc/net/xuset/tSquare/system/sound/AndroidSound.java
@@ -9,35 +9,40 @@ public class AndroidSound implements ISound {
 	private static final int maxSounds = 10;
 	private static SoundPool soundPool = createPool();
 	
-	private final int soundId;
+	private final int systemSoundId;
+	private int tempSoundId = 0;
 	
 	AndroidSound(AssetFile file) {
 		AssetFileDescriptor fd = file.getFileDescriptor();
 		if (fd == null)
 			throw new RuntimeException("Could not open file descriptor");
-		soundId = soundPool.load(fd, 1);
+		systemSoundId = soundPool.load(fd, 1);
 	}
 
 	AndroidSound(String url) {
-		soundId = soundPool.load(url, 1);
+		systemSoundId = soundPool.load(url, 1);
 	}
 	
 	@Override
 	public void play() {
 		if (!SoundStore.isMuteOn()) {
-			stop();
-			soundPool.play(soundId, 100, 100, 6, 0, 1.0f);
+			if (tempSoundId != 0)
+				stop();
+			
+			tempSoundId = soundPool.play(systemSoundId, 100, 100, 6, 0, 1.0f);
+			System.out.println("STARTING: " + tempSoundId);
 		}
 	}
 
 	@Override
 	public void stop() {
-		soundPool.stop(soundId);
+		System.out.println("STOPING: " + tempSoundId);
+		soundPool.stop(tempSoundId);
 	}
 
 	@Override
 	public void close() {
-		soundPool.unload(soundId);
+		soundPool.unload(systemSoundId);
 	}
 	
 	public static void releaseResources() {
