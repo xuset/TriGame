d405ba6d79398020a87d2f123cd9718f5d71e13b
Austin Middleton <xuset1@gmail.com>
2014-05-09 17:11:53 -0500

    Added + and - buttons to zoom settings

--- commonSrc/net/xuset/triGame/settings/UiSettingsZoom.java
+++ commonSrc/net/xuset/triGame/settings/UiSettingsZoom.java
@@ -1,11 +1,15 @@
 package net.xuset.triGame.settings;
 
+import net.xuset.tSquare.system.input.mouse.MouseAction;
+import net.xuset.tSquare.system.input.mouse.TsMouseEvent;
 import net.xuset.tSquare.ui.Alignment;
 import net.xuset.tSquare.ui.Axis;
+import net.xuset.tSquare.ui.UiButton;
 import net.xuset.tSquare.ui.UiForm;
 import net.xuset.tSquare.ui.UiLabel;
 import net.xuset.tSquare.ui.UiSlider;
 import net.xuset.tSquare.ui.UiSlider.SliderChange;
+import net.xuset.tSquare.util.Observer;
 
 class UiSettingsZoom extends UiForm {
 	private static final double minZoom = 0.5, maxZoom = 2.0;
@@ -18,10 +22,17 @@ class UiSettingsZoom extends UiForm {
 
 		this.listener = sliderListener;
 		
+		UiButton btnSub = new UiButton(" - ");
+		UiButton btnAdd = new UiButton(" + ");
+		btnSub.addMouseListener(new BtnSubAction());
+		btnAdd.addMouseListener(new BtnAddAction());
+		
 		getLayout().setAlignment(Axis.Y_AXIS, Alignment.CENTER);
 		
 		getLayout().add(new UiLabel(name + "   "));
+		getLayout().add(btnSub);
 		getLayout().add(slider);
+		getLayout().add(btnAdd);
 		getLayout().add(lblZoom);
 		
 		setInitialPosition(init);
@@ -49,6 +60,32 @@ class UiSettingsZoom extends UiForm {
 		@Override
 		public void onChange(double newPosition) {
 			reset();
+		}
+	}
+	
+	private void incrimentZoom(int incr) {
+		double p = slider.getProgress();
+		int seg = 30;
+		double newProg = p + 0.5 / seg + incr / ((double) seg);
+		newProg = Math.floor(newProg * seg) / seg;
+		newProg = Math.min(1, Math.max(0, newProg));
+		slider.setProgress(newProg);
+		reset();
+	}
+	
+	private class BtnSubAction implements Observer.Change<TsMouseEvent> {
+		@Override
+		public void observeChange(TsMouseEvent t) {
+			if (t.action == MouseAction.RELEASE)
+				incrimentZoom(-1);
+		}
+	}
+	
+	private class BtnAddAction implements Observer.Change<TsMouseEvent> {
+		@Override
+		public void observeChange(TsMouseEvent t) {
+			if (t.action == MouseAction.RELEASE)
+				incrimentZoom(1);
 		}
 	}
 }
